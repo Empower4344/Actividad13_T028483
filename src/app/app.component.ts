28eb0dfa8400cd6b805ff36983ee9800d4670507
Empower4344 <60904708+Empower4344@users.noreply.github.com>
2020-02-24 02:40:51 -0800

    Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {textareaAutoResize} from 'materialize-css';
+
+declare let $: any;
+declare const M;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-  title = 'Actividad3';
+export class AppComponent implements OnInit {
+  Titulos = [];
+  Notas = [];
+
+  ngOnInit(): void {
+    $('.collapsible').collapsible();
+  }
+
+  CreateNote(Titulo: HTMLInputElement, Contenido: HTMLInputElement) {
+    if (Titulo.value === '' || Contenido.value === '') {
+      alert('No se puede dejar ningun espacio en blanco');
+    } else {
+      this.Titulos.unshift(Titulo.value);
+      this.Notas.unshift(Contenido.value);
+    }
+    Titulo.value = null;
+    Contenido.value = null;
+    $('textarea#Contenido').val(null);
+    M.textareaAutoResize($('#Contenido'));
+  }
+
 }
